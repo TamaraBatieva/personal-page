21d13336b96324ac57d4c5c099fdffad12890bd4
Tamara Batieva <batieva.tamara@gmail.com>
2016-03-26 00:14:04 +0300

    change fonts for headings & make skills and courses lists the same size

--- js/script.js
+++ js/script.js
@@ -17,3 +17,19 @@ $(function () {
 //        },
 //        offset: '95%'
 //    });
+backspace: function(curString, curStrPos){
+    
+
+    setTimeout(function() {
+
+            // check string array position
+            // on the first string, only delete one word
+            // the stopNum actually represents the amount of chars to
+            // keep in the current string. In my case it's 3.
+            if (self.arrayPos == 2){
+                self.stopNum = 4;
+            }
+            //every other time, delete the whole typed string
+            else{
+                self.stopNum = 0;
+            }
